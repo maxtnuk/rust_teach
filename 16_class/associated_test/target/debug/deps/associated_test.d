203bbfddabe7b2a8480dbc1467eaec0136e51c79
maxtnuk <maxtnuk@gmail.com>
2017-01-06 23:47:00 +0900

    16_class

---
+++ 16_class/associated_test/target/debug/deps/associated_test.d
@@ -0,0 +1,5 @@
+/home/maxtnt/rust_teach/16_class/associated_test/target/debug/deps/libassociated_test.rlib: src/lib.rs
+
+/home/maxtnt/rust_teach/16_class/associated_test/target/debug/deps/associated_test.d: src/lib.rs
+
+src/lib.rs:
